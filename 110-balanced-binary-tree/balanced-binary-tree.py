5151fdb8a31b398731d5e69351682f2eb76dc814
Muriki Sai Srinivas <58871008+MurikiSaiSrinivas@users.noreply.github.com>
2025-05-08 11:29:54 -0700

    Time: 7 ms (53.53%) | Memory: 18 MB (42.15%) - LeetSync

---
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isBalanced(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: bool
+        """
+        self.balance = True
+        def dfs(root):
+            if not root:
+                return 0
+            left = dfs(root.left)
+            right = dfs(root.right)
+
+            if left-right not in [-1,0,1]:
+                self.balance = False
+            
+            return 1 + max(left, right)
+        
+        dfs(root)
+        return self.balance
