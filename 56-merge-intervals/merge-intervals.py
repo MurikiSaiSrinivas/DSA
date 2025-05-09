14ee38de22d5bad64a902bbbbede0711d2951790
Muriki Sai Srinivas <58871008+MurikiSaiSrinivas@users.noreply.github.com>
2025-05-08 19:50:59 -0700

    Time: 7 ms (96.85%) | Memory: 17.1 MB (14.14%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -4,13 +4,13 @@ class Solution(object):
         :type intervals: List[List[int]]
         :rtype: List[List[int]]
         """
-        intervals = sorted(intervals)
-        i = 0
-        while i < len(intervals)-1:
-            if intervals[i][1] >= intervals[i+1][0]:
-                values = sorted(intervals[i] + intervals[i+1])
-                intervals = intervals[:i] + intervals[i+1:]
-                intervals[i] = [values[0], values[-1]]
+        intervals.sort(key=lambda x : x[0])
+        output = [intervals[0]]
+
+        for start, end in intervals[1:]:
+            lastEnd = output[-1][1]
+            if start <= lastEnd:
+                output[-1][1] = max(lastEnd, end)
             else:
-                i+=1
-        return intervals
+                output.append([start, end])
+        return output
