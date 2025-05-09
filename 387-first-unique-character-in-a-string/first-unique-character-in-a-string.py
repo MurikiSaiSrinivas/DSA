f4643292db650ebc7ba4abbe1d8357f594937c52
Muriki Sai Srinivas <58871008+MurikiSaiSrinivas@users.noreply.github.com>
2025-05-09 10:13:19 -0700

    Time: 69 ms (91.76%) | Memory: 13 MB (50.36%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
@@ -5,10 +5,12 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        result = 0
         freq_map = defaultdict(int)
-        for index, char in enumerate(s):
+        for char in s:
             freq_map[char] += 1
-            while result < len(s) and freq_map[s[result]] > 1:
-                result += 1
-        return result if result < len(s) else -1
+        
+        for index, char in enumerate(s):
+            if freq_map[char] == 1:
+                return index
+        return -1
+        
