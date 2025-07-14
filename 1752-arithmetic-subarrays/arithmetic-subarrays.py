21c5611435f06dbf773574278d16ae1c09647258
Muriki Sai Srinivas <58871008+MurikiSaiSrinivas@users.noreply.github.com>
2025-07-13 20:33:13 -0700

    Time: 35 ms (77.78%) | Memory: 17.9 MB (82.38%) - LeetSync

---
+++ 1752-arithmetic-subarrays/arithmetic-subarrays.py
@@ -0,0 +1,17 @@
+class Solution:
+    def checkArithmeticSubarrays(self, nums: List[int], l: List[int], r: List[int]) -> List[bool]:
+        m = len(l)
+        result = []
+        for i in range(m):
+            new_sorted_array = sorted(nums[l[i]: r[i]+1])
+
+            value_change = new_sorted_array[1] - new_sorted_array[0]
+            holder = True
+
+            for num_index in range(len(new_sorted_array)-1):
+                if new_sorted_array[num_index+1] - new_sorted_array[num_index] != value_change:
+                    holder =False
+                    break
+                    
+            result.append(holder)
+        return result
