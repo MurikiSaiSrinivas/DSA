e9b2a83c51e9e1c1283fca11984209d75c66cc95
Muriki Sai Srinivas <58871008+MurikiSaiSrinivas@users.noreply.github.com>
2025-12-13 23:44:01 -0800

    Time: 4 ms (36.91%) | Memory: 49.3 MB (68.61%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int PivotIndex(int[] nums) {
+        int total_sum = nums.Sum();
+        int left_sum = 0;
+        for (int i = 0; i < nums.Length; i++) {
+            int right_sum = total_sum - left_sum - nums[i];
+            if (left_sum == right_sum) {
+                return i;
+            }
+            left_sum += nums[i];
+        }
+        return -1;
+    }
+}
